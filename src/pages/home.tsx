ae7108938cb0425b9128d5531810e971a5dfb32d
18297920312 <2635627340@qq.com>
2022-07-03 23:31:16 +0800

    hemain布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './home.scss';
 import headerBg from '../images/header.png';
+import {Redirect} from "react-router-dom";
 export const Home = () => {
   return (
-    <div id='header' style={{backgroundImage: `url(${headerBg})`}}></div>
-  );
+   <div className='home'>
+      <div className='header' style={{backgroundImage: `url(${headerBg})`}}></div>
+      <main className='main'>
+        <section  className="section1"></section>
+        <section  className="section2"></section>
+        <section  className="section3"></section>
+        <section  className="section4"></section>
+      </main>
+   </div>
+);
 };
